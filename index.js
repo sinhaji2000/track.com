b22e1b1945301f970aad255e881256f3f7232475
sinhaji2000 <sinhaoffcial@gmail.com>
2024-12-29 00:43:56 +0530

    add tailwind css using posecss

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static("./assests"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./views/**/*.{html,ejs}", "./assets/**/*.{html,js}"],
   theme: {
     extend: {},
   },
